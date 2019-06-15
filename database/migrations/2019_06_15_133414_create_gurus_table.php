0577ff70b68d05a077768dc578dcabb152e7869f
AKSi Plus <antoniosai@aksiplus>
2019-06-15 23:13:42 +0700

    Mengganti nama migration guru

--- database/migrations/2019_06_15_133414_create_gurus_table.php
+++ database/migrations/2019_06_15_133414_create_gurus_table.php
@@ -13,7 +13,7 @@ class CreateGurusTable extends Migration
      */
     public function up()
     {
-        Schema::create('guru', function (Blueprint $table) {
+        Schema::create('tenaga_pendidik', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
@@ -31,6 +31,6 @@ class CreateGurusTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('guru');
+        Schema::dropIfExists('tenaga_pendidik');
     }
 }
